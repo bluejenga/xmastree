4add75668f53e7d8cb9af96e0c892a6ce16935a0
Atsushi USUI <bluejenga@gmail.com>
2019-09-26 13:58:38 +0900

    PEP8

--- tree.py
+++ tree.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import random
 import itertools
 import sys
 
-#Direction
-DIR_N = 0 #North
-DIR_E = 1 #East
-DIR_S = 2 #South
-DIR_W = 3 #West
+# Direction
+DIR_N = 0  # North
+DIR_E = 1  # East
+DIR_S = 2  # South
+DIR_W = 3  # West
 DIR_NUM = 4
-#隣のセルへの座標の差分
+# 隣のセルへの座標の差分
 DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
 
 class Cell:
     """Treeを構成するCellのクラス"""
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        #迷路の一部に割り当てられたかどうか
+        # 迷路の一部に割り当てられたかどうか
         self.used = False
-        #隣接するセルと接続している方角
-        self.linked_dir = [False, False, False, False] #[N, E, S, W]
+        # 隣接するセルと接続している方角
+        self.linked_dir = [False, False, False, False]  # [N, E, S, W]
         self.light = False
 
     def rotate(self, n):
@@ -39,7 +40,7 @@ class Tree:
     """
     def __init__(self, height):
         self.height = height
-        #widthはheightから算出する。
+        # widthはheightから算出する。
         width = height * 2 - 3 if height > 1 else 1
         self.width = width
 
@@ -47,29 +48,27 @@ class Tree:
         for y, x in itertools.product(range(height), range(width)):
             if self.is_valid_coord(x, y):
                 self.cells[x + y * width] = (Cell(x, y))
-        #有効なセルの数
+        # 有効なセルの数
         self.cellnum = height * height - 2 * height + 2
-        #点灯しているセルの数
+        # 点灯しているセルの数
         self.lightcellnum = 0
 
-        #根元、幹のCell
+        # 根元、幹のCell
         center = self.height - 2 if self.height > 1 else 0
         self.root = self.cells[center + (height - 1) * width]
-
-
 
     def is_valid_coord(self, x, y):
         """
         指定した座標が有効かどうかを返す。
         """
 
-        #長方形の範囲に収まるかどうか
+        # 長方形の範囲に収まるかどうか
         if x < 0 or x >= self.width:
             return False
         if y < 0 or y >= self.height:
             return False
 
-        #木の形の範囲に収まるかどうか
+        # 木の形の範囲に収まるかどうか
         center = self.height - 2 if self.height > 1 else 0
         if x + y < center:
             return False
@@ -79,7 +78,6 @@ class Tree:
             return False
 
         return True
-
 
     def get_next_cell(self, cell, direction):
         """指定した方角の隣接するセルを取得する"""
@@ -94,52 +92,52 @@ class Tree:
         links = []
         for direction in range(DIR_NUM):
             dst = self.get_next_cell(src, direction)
-            if dst != None and dst.used == False:
+            if dst is not None and not dst.used:
                 links.append((src, direction, dst))
         return links
 
     def build(self):
         """迷路を構築する"""
 
-        #初期化
+        # 初期化
         for cell in self.cells:
-            if cell != None:
+            if cell is not None:
                 cell.used = False
                 cell.linked_dir = [False, False, False, False]
                 cell.light = False
         self.lightcellnum = 0
 
-        #木の根元から迷路作成開始（実際にはどこからでもよい）
+        # 木の根元から迷路作成開始（実際にはどこからでもよい）
         self.root.used = True
         links = self.get_available_links_from(self.root)
 
-        #通路候補がなくなるまで繰り返し
+        # 通路候補がなくなるまで繰り返し
         while len(links) > 0:
 
-            #候補から通路を一つ選択
+            # 候補から通路を一つ選択
             src, direction, dst = random.choice(links)
             dst.used = True
             src.linked_dir[direction] = True
             dst.linked_dir[(direction + 2) % 4] = True
 
-            #既存候補の中から、dstが重複しているものを削除
+            # 既存候補の中から、dstが重複しているものを削除
             links = [ln for ln in links if ln[2] != dst]
-            #dstを起点とした通路を候補に追加
+            # dstを起点とした通路を候補に追加
             links.extend(self.get_available_links_from(dst))
 
     def print_tree(self):
         """コンソールに木の絵を出力"""
 
         def getString(cell):
-            if cell == None:
+            if cell is None:
                 return ['     '] * 3
 
             n = '   ' if cell.linked_dir[DIR_N] else '---'
             s = '   ' if cell.linked_dir[DIR_S] else '---'
             w = ' ' if cell.linked_dir[DIR_W] else '|'
             e = ' ' if cell.linked_dir[DIR_E] else '|'
-            l = 'X' if cell.light else ' '
-            return [f'+{n}+', f'{w} {l} {e}', f'+{s}+']
+            L = 'X' if cell.light else ' '
+            return [f'+{n}+', f'{w} {L} {e}', f'+{s}+']
 
         print('  ', end='')
         for x in range(self.width):
@@ -158,11 +156,10 @@ class Tree:
                     print(cellstr[x + y * self.width][i], end='')
                 print()
 
-
     def shuffle(self):
         """各セルの向きをシャッフルする"""
         for cell in self.cells:
-            if cell != None and cell is not self.root:
+            if cell is not None and cell is not self.root:
                 n = random.choice(range(DIR_NUM))
                 cell.rotate(n)
 
@@ -174,29 +171,29 @@ class Tree:
 
     def lightup(self):
         """根元から辿って、繋がっているセルを点灯する"""
-        #一旦すべて消灯
+        # 一旦すべて消灯
         self.lightcellnum = 0
         for cell in self.cells:
-            if cell != None:
+            if cell is not None:
                 cell.light = False
 
         self._lightup_recursive(self.root)
 
     def _lightup_recursive(self, cell):
         """lightup()の内部で再帰的に呼び出す関数"""
-        #まずは点灯
+        # まずは点灯
         cell.light = True
         self.lightcellnum = self.lightcellnum + 1
 
-        #通路が伸びている方角を取得
+        # 通路が伸びている方角を取得
         dirs = [d for d, ln in enumerate(cell.linked_dir) if ln]
-        #その方角のセルを取得
+        # その方角のセルを取得
         lncells = [self.get_next_cell(cell, d) for d in dirs]
-        #そのセルからも自分の方に通路が伸びていれば、再帰的に点灯処理
+        # そのセルからも自分の方に通路が伸びていれば、再帰的に点灯処理
         for d, lncell in zip(dirs, lncells):
-            if (lncell != None
-                    and lncell.linked_dir[(d + 2) % 4] == True
-                    and lncell.light == False):
+            if (lncell is not None
+                    and lncell.linked_dir[(d + 2) % 4]
+                    and not lncell.light):
                 self._lightup_recursive(lncell)
 
     def is_complete(self):
@@ -234,11 +231,11 @@ if __name__ == '__main__':
         else:
             coord = str.split()
             if (len(coord) != 2
-                    or coord[0].isdecimal == False
-                    or coord[1].isdecimal == False):
+                    or not coord[0].isdecimal
+                    or not coord[1].isdecimal):
                 continue
             x, y = (int(s) for s in coord)
-            if tree.is_valid_coord(x, y) == False:
+            if not tree.is_valid_coord(x, y):
                 continue
             tree.rotate(int(coord[0]), int(coord[1]))
 
